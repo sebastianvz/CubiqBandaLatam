980f6d7c7c4074fc2656834e242e3bbd90db1a77
Sebastián Velez <devsebastianvz@gmail.com>
2020-01-24 16:07:53 -0500

    base

---
+++ formBandaLatam/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace formBandaLatam
+{
+    partial class FormCubiq
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCubiq));
+            this.lblAlto = new System.Windows.Forms.Label();
+            this.lblAncho = new System.Windows.Forms.Label();
+            this.lblLargo = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAlto
+            // 
+            this.lblAlto.AutoSize = true;
+            this.lblAlto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblAlto.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlto.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAlto.Location = new System.Drawing.Point(81, 38);
+            this.lblAlto.Name = "lblAlto";
+            this.lblAlto.Size = new System.Drawing.Size(91, 37);
+            this.lblAlto.TabIndex = 0;
+            this.lblAlto.Text = "Alto :";
+            // 
+            // lblAncho
+            // 
+            this.lblAncho.AutoSize = true;
+            this.lblAncho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblAncho.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAncho.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAncho.Location = new System.Drawing.Point(81, 98);
+            this.lblAncho.Name = "lblAncho";
+            this.lblAncho.Size = new System.Drawing.Size(127, 37);
+            this.lblAncho.TabIndex = 1;
+            this.lblAncho.Text = "Ancho :";
+            // 
+            // lblLargo
+            // 
+            this.lblLargo.AutoSize = true;
+            this.lblLargo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblLargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLargo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblLargo.Location = new System.Drawing.Point(81, 161);
+            this.lblLargo.Name = "lblLargo";
+            this.lblLargo.Size = new System.Drawing.Size(118, 37);
+            this.lblLargo.TabIndex = 2;
+            this.lblLargo.Text = "Largo :";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPeso.Location = new System.Drawing.Point(81, 225);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(107, 37);
+            this.lblPeso.TabIndex = 3;
+            this.lblPeso.Text = "Peso :";
+            // 
+            // txtAlto
+            // 
+            this.txtAlto.Enabled = false;
+            this.txtAlto.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAlto.Location = new System.Drawing.Point(207, 31);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(229, 44);
+            this.txtAlto.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(207, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 44);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(207, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 44);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(207, 218);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(229, 44);
+            this.textBox3.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.lblAlto);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.lblAncho);
+            this.groupBox1.Controls.Add(this.txtAlto);
+            this.groupBox1.Controls.Add(this.lblLargo);
+            this.groupBox1.Controls.Add(this.lblPeso);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Location = new System.Drawing.Point(360, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 375);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Medidas";
+            // 
+            // FormCubiq
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCubiq";
+            this.Text = "Cubiq Banda";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAlto;
+        private System.Windows.Forms.Label lblAncho;
+        private System.Windows.Forms.Label lblLargo;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtAlto;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
